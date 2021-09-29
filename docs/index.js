bd3bbfac90a05cba95459d1ab7f7a5fe7cde4b43
seoyoenkk68 <seoyeonkk68@gmail.com>
2021-09-29 17:23:52 +0900

    제품 정보 슬라이더 작업중

---
+++ docs/index.js
@@ -0,0 +1,19 @@
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //slidesPerView: 4,
+  //spaceBetween: 30,
+});
